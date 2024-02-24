751e9666c3a3e46489e53940538046e6ea7e2602
Yulia Prusova <prusova108@gmail.com>
2024-02-24 15:52:21 +0300

    finish

--- hw02_unpack_string/main.go
+++ hw02_unpack_string/main.go
@@ -7,10 +7,16 @@ import (
 )
 
 func main() {
-	var data = [...]string{"a4bc2d5e", "abcd", "aaa0b", "d\n5abc", "qwe\\45", "qwe\\4\\5"}
-	// var data = [...]string{"a4bc2d5e"}
+	var data = [...]string{"a4bc2d5e", "abcd", "aaa0b", "d\n5abc", "qwe\\45", "qwe\\4\\5", "qwe\\\\5"}
+	// var data = [...]string{"qwe\\\\5"}
 	for i, el := range data {
 		formatted_str, _ := hw02unpackstring.Unpack(el)
 		fmt.Println(i, el, "=>", formatted_str)
+	}
+	fmt.Println("---\nErrors")
+	var err_data = [...]string{"3abc", "45", "aaa10b"}
+	for i, el := range err_data {
+		_, err := hw02unpackstring.Unpack(el)
+		fmt.Println(i, el, "=>", err)
 	}
 }
