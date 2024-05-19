92db66de91775342bd9f06e62236e3ffd8714e21
Yulia Prusova <prusova108@gmail.com>
2024-05-19 22:29:36 +0300

    Init task

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,27 @@ type (
 
 type Stage func(in In) (out Out)
 
+func stage_wrapper(in In, done In, stage Stage) Out {
+	out := make(Bi)
+	stage_out := stage(in)
+	go func() {
+		defer close(out)
+		for i := range stage_out {
+			select {
+			case <-done:
+				return
+			case out <- i:
+			}
+		}
+	}()
+	return out
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	stage_in := in
+	for _, stage := range stages {
+		// stage_in = stage(stage_in)
+		stage_in = stage_wrapper(stage_in, done, stage)
+	}
+	return stage_in
 }
