7163a64fc93fcff255d3b41ad57097605782a97c
Yulia Prusova <prusova108@gmail.com>
2024-05-21 18:49:24 +0300

    Add func openFromFile and tests

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"os"
 	"testing"
 
@@ -12,9 +13,31 @@ func TestCopy(t *testing.T) {
 }
 
 func TestOpenFromFile(t *testing.T) {
+	file_name := "test_file"
+	file_content := "Package os provides a platform-independent interface to operating system functionality. The design is Unix-like, although the error handling is Go-like; failing calls return values of type error rather than error numbers. Often, more information is available within the error. For example, if a call that takes a file name fails, such as Open or Stat, the error will include the failing file name when printed and will be of type *PathError, which may be unpacked for more information."
 	t.Run("success_opening", func(t *testing.T) {
-		file_name := "test_file"
-		_, err := os.Create(file_name)
+		if _, err := os.Stat(file_name); err == nil {
+			if err := os.Remove(file_name); err != nil {
+				t.FailNow()
+			}
+		} else if !errors.Is(err, os.ErrNotExist) {
+			t.FailNow()
+		}
+		file, err := os.Create(file_name)
+		if err != nil {
+			t.FailNow()
+		}
+		file.WriteString(file_content)
+		file.Close()
+		offset := int(len(file_content) / 2)
+		_, err = openFromFile(file_name, int64(offset))
 		require.Nil(t, err)
+		os.Remove(file_name)
 	})
+
+	// t.Run("file_not_exist", func(t *testing.T) {
+	// 	if _, err := os.Stat(); err == nil {
+
+	// 	}
+	// })
 }
