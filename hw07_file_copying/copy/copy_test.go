848cb5bc19f49c7c395541cac3b63df87d84fb64
Yulia Prusova <prusova108@gmail.com>
2024-05-29 22:26:09 +0300

    add tests for func offsetPrepare

--- hw07_file_copying/copy/copy_test.go
+++ hw07_file_copying/copy/copy_test.go
@@ -1,6 +1,7 @@
 package copy
 
 import (
+	"errors"
 	"io"
 	"io/fs"
 	"testing"
@@ -27,16 +28,57 @@ func (o *StatMock) Size() int64 {
 
 func TestOffsetPrepare(t *testing.T) {
 	offset := 100
-	t.Run("test_success", func(t *testing.T) {
+
+	cases := []struct {
+		name string
+		size int
+	}{
+		{name: "size_eq_offset", size: offset},
+		{name: "size_gt_offset", size: offset + 1},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			stat_mock := &StatMock{}
+			stat_mock.On("Size").Return(tc.size)
+			file_mock := mocks.NewFile(t)
+			file_mock.EXPECT().Stat().Return(stat_mock, nil)
+			file_mock.EXPECT().Seek(int64(offset), io.SeekStart).Return(int64(0), nil)
+
+			err := offsetPrepare(file_mock, int64(offset))
+			require.Nil(t, err)
+		})
+	}
+
+	t.Run("size_lt_offset", func(t *testing.T) {
+		stat_mock := &StatMock{}
+		stat_mock.On("Size").Return(offset - 1)
+		file_mock := mocks.NewFile(t)
+		file_mock.EXPECT().Stat().Return(stat_mock, nil)
+
+		err := offsetPrepare(file_mock, int64(offset))
+		require.ErrorIs(t, err, ErrOffsetExceedsFileSize)
+	})
+
+	t.Run("stat_error", func(t *testing.T) {
+		file_mock := mocks.NewFile(t)
+		file_mock.EXPECT().Stat().Return(nil, errors.New("Test error"))
+
+		err := offsetPrepare(file_mock, int64(offset))
+		require.ErrorIs(t, err, ErrUnsupportedFile)
+	})
+
+	t.Run("seek_error", func(t *testing.T) {
 		stat_mock := &StatMock{}
 		stat_mock.On("Size").Return(offset)
 		file_mock := mocks.NewFile(t)
 		file_mock.EXPECT().Stat().Return(stat_mock, nil)
-		file_mock.EXPECT().Seek(int64(offset), io.SeekStart).Return(int64(0), nil)
+		returned_err := errors.New("Test error")
+		file_mock.EXPECT().Seek(int64(offset), io.SeekStart).Return(0, returned_err)
 
-		error := offsetPrepare(file_mock, int64(offset))
-		require.Nil(t, error)
-
+		err := offsetPrepare(file_mock, int64(offset))
+		require.ErrorIs(t, err, returned_err)
 	})
 
 }
