5351dce5c224b759e99be4adca2ae69ecb91cd02
Yulia Prusova <prusova108@gmail.com>
2024-05-31 21:37:53 +0300

    fix tests

--- hw07_file_copying/dd_copy/copy_test.go
+++ hw07_file_copying/dd_copy/copy_test.go
@@ -138,62 +138,6 @@ func TestSeek(t *testing.T) {
 	})
 }
 
-// func TestOffsetPrepare(t *testing.T) {
-// 	offset := 100
-
-// 	cases := []struct {
-// 		name string
-// 		size int
-// 	}{
-// 		{name: "size_eq_offset", size: offset},
-// 		{name: "size_gt_offset", size: offset + 1},
-// 	}
-
-// 	for _, tc := range cases {
-// 		tc := tc
-// 		t.Run(tc.name, func(t *testing.T) {
-// 			stat_mock := &StatMock{}
-// 			stat_mock.On("Size").Return(tc.size)
-// 			file_mock := mocks.NewFile(t)
-// 			file_mock.EXPECT().Stat().Return(stat_mock, nil)
-// 			file_mock.EXPECT().Seek(int64(offset), io.SeekStart).Return(int64(0), nil)
-
-// 			err := seek(file_mock, int64(offset))
-// 			require.Nil(t, err)
-// 		})
-// 	}
-
-// 	t.Run("size_lt_offset", func(t *testing.T) {
-// 		stat_mock := &StatMock{}
-// 		stat_mock.On("Size").Return(offset - 1)
-// 		file_mock := mocks.NewFile(t)
-// 		file_mock.EXPECT().Stat().Return(stat_mock, nil)
-
-// 		err := seek(file_mock, int64(offset))
-// 		require.ErrorIs(t, err, ErrOffsetExceedsFileSize)
-// 	})
-
-// 	t.Run("stat_error", func(t *testing.T) {
-// 		file_mock := mocks.NewFile(t)
-// 		file_mock.EXPECT().Stat().Return(nil, errors.New("Test error"))
-
-// 		err := seek(file_mock, int64(offset))
-// 		require.ErrorIs(t, err, ErrUnsupportedFile)
-// 	})
-
-// 	t.Run("seek_error", func(t *testing.T) {
-// 		stat_mock := &StatMock{}
-// 		stat_mock.On("Size").Return(offset)
-// 		file_mock := mocks.NewFile(t)
-// 		file_mock.EXPECT().Stat().Return(stat_mock, nil)
-// 		returned_err := errors.New("Test error")
-// 		file_mock.EXPECT().Seek(int64(offset), io.SeekStart).Return(0, returned_err)
-
-// 		err := seek(file_mock, int64(offset))
-// 		require.ErrorIs(t, err, returned_err)
-// 	})
-// }
-
 func TestReadWrite(t *testing.T) {
 	limit := 10
 	read_byte := byte(1)
