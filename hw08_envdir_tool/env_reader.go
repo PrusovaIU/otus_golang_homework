42fe3e7a69909808b31b361db7aec17bcb229846
Yulia Prusova <prusova108@gmail.com>
2024-06-04 22:59:56 +0300

    add functions for read dir with files

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -8,9 +17,63 @@ type EnvValue struct {
 	NeedRemove bool
 }
 
+func NewEnvValue(value string, needRemove bool) EnvValue {
+	return EnvValue{
+		Value:      value,
+		NeedRemove: needRemove,
+	}
+}
+
+type EnvFile interface {
+	ReadLine() (line []byte, isPrefix bool, err error)
+}
+
+func readFile(file EnvFile) (EnvValue, error) {
+	line, _, err := file.ReadLine()
+	if err != nil {
+		return NewEnvValue("", false), err
+	}
+	if len(line) == 0 {
+		return NewEnvValue("", true), nil
+	} else {
+		return NewEnvValue(string(line), false), nil
+	}
+}
+
+type EnvFileInfo interface {
+	Name() string
+}
+
+func getParametername(info EnvFileInfo) string {
+	fileName := info.Name()
+	forbidden_symbols := regexp.MustCompile(`[= ]`)
+	fileName = forbidden_symbols.ReplaceAllString(fileName, "")
+	return strings.ReplaceAll(fileName, "0x00", "")
+}
+
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	envMap := Environment{}
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Printf("panic by handling failure accessing a path %q: %v\n", path, err)
+			return err
+		}
+		if path != dir {
+			file, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			defer file.Close()
+			envValue, err := readFile(bufio.NewReader(file))
+			if err != nil {
+				return err
+			}
+			envName := getParametername(info)
+			envMap[envName] = envValue
+		}
+		return nil
+	})
+	return nil, err
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	env_dir_path := os.Args[1]
+	command := os.Args[2]
+	args := os.Args[3:]
+
+	fmt.Println(env_dir_path, command, args)
+	fmt.Println(ReadDir(env_dir_path))
 }
