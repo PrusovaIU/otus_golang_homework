ccb4e9147e9ea81014ddc4110ffd9dc58e2b26ef
Yulia Prusova <prusova108@gmail.com>
2024-06-06 17:56:17 +0300

    Add test for func readFile

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,6 +1,25 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/PrusovaIU/otus_golang_homework/hw08_envdir_tool/mocks"
+	"github.com/stretchr/testify/require"
+)
+
+func TestReadFile(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		fileContent := "test_value"
+		fileMock := mocks.NewEnvFile(t)
+		fileMock.EXPECT().ReadLine().Return([]byte(fileContent), false, nil)
+
+		result, err := readFile(fileMock)
+		require.NoError(t, err)
+		require.Equal(t, fileContent, result.Value)
+		require.False(t, result.NeedRemove)
+	})
+
+}
 
 func TestReadDir(t *testing.T) {
 	// Place your code here
