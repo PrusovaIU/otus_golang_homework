6b390106bda9d6caf2c9fa39c18583229fb82ec0
Yulia Prusova <prusova108@gmail.com>
2024-06-12 16:03:36 +0300

    add tests for func formEnv

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,57 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func formEnv(dirEnv Environment) error {
+	for name, value := range dirEnv {
+		if value.NeedRemove {
+			if err := os.Unsetenv(name); err != nil {
+				return err
+			}
+		} else {
+			if err := os.Setenv(name, value.Value); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+type Process interface {
+	Start() error
+	Wait() error
+}
+
+func processManage(process Process) error {
+	if err := process.Start(); err != nil {
+		return err
+	}
+	if err := process.Wait(); err != nil {
+		return err
+	}
+	return nil
+}
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	checkErr := func(err error) int {
+		fmt.Println(err)
+		return 1
+	}
+	if err := formEnv(env); err != nil {
+		return checkErr(err)
+	}
+	process := exec.Command(cmd[0], cmd[1:]...)
+	process.Env = os.Environ()
+	process.Stderr = os.Stderr
+	process.Stdout = os.Stdout
+	process.Stdin = os.Stdin
+	if err := processManage(process); err != nil {
+		return checkErr(err)
+	}
+	return process.ProcessState.ExitCode()
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,6 +1,53 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestFormEnv(t *testing.T) {
+	paramName := "test"
+	cleanEnv := func(paramName string) {
+		if _, exists := os.LookupEnv(paramName); exists {
+			os.Unsetenv(paramName)
+		}
+	}
+	t.Run("test_need_remove", func(t *testing.T) {
+		err := os.Setenv(paramName, "test_value")
+		require.NoError(t, err)
+		defer cleanEnv(paramName)
+		envValue := NewEnvValue("", true)
+		env := Environment{paramName: envValue}
+		err = formEnv(env)
+		require.NoError(t, err)
+		_, exists := os.LookupEnv(paramName)
+		require.False(t, exists)
+	})
+
+	test_not_remove := func(t *testing.T) {
+		defer cleanEnv(paramName)
+		exValue := "test_value"
+		envValue := NewEnvValue(exValue, false)
+		env := Environment{paramName: envValue}
+		err := formEnv(env)
+		require.NoError(t, err)
+		value, exists := os.LookupEnv(paramName)
+		require.True(t, exists)
+		require.Equal(t, exValue, value)
+	}
+
+	t.Run("test_exists", func(t *testing.T) {
+		err := os.Setenv(paramName, "old_value")
+		require.NoError(t, err)
+		test_not_remove(t)
+	})
+
+	t.Run("test_not_exists", func(t *testing.T) {
+		test_not_remove(t)
+	})
+}
 
 func TestRunCmd(t *testing.T) {
 	// Place your code here
