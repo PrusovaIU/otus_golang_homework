1305ff15fdc2440a0159f8434d59e6521c4d1473
Yulia Prusova <prusova108@gmail.com>
2024-10-27 23:59:42 +0300

    промежуточный коммит

---
+++ hw09_struct_validator/validation_error.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type ValidationError struct {
+	Field string
+	Err   error
+}
+
+func (v ValidationError) Error() string {
+	return fmt.Sprintf("\tfield %s %s\n", v.Field, v.Err)
+}
+
+type ValidationErrors []ValidationError
+
+func (v ValidationErrors) Error() string {
+	var sb strings.Builder
+	for _, err := range v {
+		sb.WriteString(err.Error())
+	}
+	return sb.String()
+}
--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,17 +1,72 @@
-package hw09structvalidator
+package main
 
-type ValidationError struct {
-	Field string
-	Err   error
-}
+import (
+	"errors"
+	"fmt"
+	"reflect"
+)
 
-type ValidationErrors []ValidationError
-
-func (v ValidationErrors) Error() string {
-	panic("implement me")
-}
-
-func Validate(v interface{}) error {
-	// Place your code here.
+func validateString(fieldValue reflect.Value, tag string) error {
 	return nil
+}
+
+func validateInt(fieldValue reflect.Value, tag string) error {
+	return nil
+}
+
+func validateSlice(fieldValue reflect.Value, fieldType reflect.StructField, tag string) []error {
+	var errs []error = []error{}
+	var validateFunc func(reflect.Value, string) error = nil
+	switch fieldType.Type.Elem().Kind() {
+	case reflect.Int:
+		validateFunc = validateInt
+	case reflect.String:
+		validateFunc = validateString
+	}
+	if validateFunc != nil {
+		for i := 0; i < fieldValue.Len(); i++ {
+			elValue := fieldValue.Index(i)
+			if err := validateFunc(elValue, tag); err != nil {
+				errs = append(errs, err)
+			}
+		}
+	}
+
+}
+
+func validateField(fieldValue reflect.Value, fieldType reflect.StructField) error {
+	isExported := fieldType.IsExported()
+	var err error = nil
+	if isExported {
+		tag := fieldType.Tag.Get("validate")
+		if len(tag) > 0 {
+			switch fieldType.Type.Kind() {
+			case reflect.String:
+				err = validateString(fieldValue, tag)
+			case reflect.Int:
+				err = validateInt(fieldValue, tag)
+			case reflect.Slice:
+				err = validateSlice(fieldValue, fieldType, tag)
+			}
+		}
+	}
+	return err
+}
+
+func Validate() error {
+	var errs ValidationErrors = []ValidationError{}
+	errs = append(errs, ValidationError{
+		Field: "field_1",
+		Err:   errors.New("test_error_1"),
+	})
+	errs = append(errs, ValidationError{
+		Field: "field_2",
+		Err:   errors.New("test_error_2"),
+	})
+	return errs
+}
+
+func main() {
+	a := Validate()
+	fmt.Println(a)
 }
