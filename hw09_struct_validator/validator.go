c282c8f4a64af0bf3ed8b82bdbba017a8f825235
Yulia Prusova <prusova108@gmail.com>
2024-10-30 22:30:00 +0300

    change function

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -14,8 +14,8 @@ func validateInt(fieldValue reflect.Value, tag string) error {
 	return nil
 }
 
-func validateSlice(fieldValue reflect.Value, fieldType reflect.StructField, tag string) []error {
-	var errs []error = []error{}
+func validateSlice(fieldValue reflect.Value, fieldType reflect.StructField, tag string) ValidationErrors {
+	var errs ValidationErrors = []ValidationError{}
 	var validateFunc func(reflect.Value, string) error = nil
 	switch fieldType.Type.Elem().Kind() {
 	case reflect.Int:
@@ -27,30 +27,51 @@ func validateSlice(fieldValue reflect.Value, fieldType reflect.StructField, tag 
 		for i := 0; i < fieldValue.Len(); i++ {
 			elValue := fieldValue.Index(i)
 			if err := validateFunc(elValue, tag); err != nil {
+				validationErr := ValidationError{
+					Field: fmt.Sprintf("%s[%d]", fieldType.Name, i),
+					Err:   err,
+				}
+				errs = append(errs, validationErr)
+			}
+		}
+	}
+	return errs
+}
+
+func validateNotSlice(fieldValue reflect.Value, fieldType reflect.Kind, fieldName string, tag string) ValidationError {
+	var err error = nil
+	var validationErr = ValidationError{}
+	switch fieldType {
+	case reflect.String:
+		err = validateString(fieldValue, tag)
+	case reflect.Int:
+		err = validateInt(fieldValue, tag)
+	}
+	if err != nil {
+		validationErr = ValidationError{
+			Field: fieldName,
+			Err:   err,
+		}
+	}
+	return validationErr
+}
+
+func validateField(fieldValue reflect.Value, fieldType reflect.StructField) ValidationErrors {
+	isExported := fieldType.IsExported()
+	var errs ValidationErrors = []ValidationError{}
+	if isExported {
+		tag := fieldType.Tag.Get("validate")
+		if len(tag) > 0 {
+			fieldKind := fieldType.Type.Kind()
+			if fieldKind == reflect.Slice {
+				errs = append(errs, validateSlice(fieldValue, fieldType, tag)...)
+			} else {
+				err := validateNotSlice(fieldValue, fieldKind, fieldType.Name, tag)
 				errs = append(errs, err)
 			}
 		}
 	}
-
-}
-
-func validateField(fieldValue reflect.Value, fieldType reflect.StructField) error {
-	isExported := fieldType.IsExported()
-	var err error = nil
-	if isExported {
-		tag := fieldType.Tag.Get("validate")
-		if len(tag) > 0 {
-			switch fieldType.Type.Kind() {
-			case reflect.String:
-				err = validateString(fieldValue, tag)
-			case reflect.Int:
-				err = validateInt(fieldValue, tag)
-			case reflect.Slice:
-				err = validateSlice(fieldValue, fieldType, tag)
-			}
-		}
-	}
-	return err
+	return errs
 }
 
 func Validate() error {
