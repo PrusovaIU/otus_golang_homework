642f0a71fefd5ae7f637924d497b0c1ee42f9efa
Yulia Prusova <prusova108@gmail.com>
2025-03-13 21:59:43 +0300

    TCPClient has been realised

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
@@ -12,9 +14,59 @@ type TelnetClient interface {
 	Receive() error
 }
 
-func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+type TCPClient struct {
+	conn    net.Conn
+	in      io.ReadCloser
+	out     io.Writer
+	address string
+	timeout time.Duration
+}
+
+func (c *TCPClient) Close() error {
+	if c.conn != nil {
+		return c.conn.Close()
+	}
 	return nil
+}
+
+func (c *TCPClient) Connect() error {
+	if c.conn == nil {
+		conn, err := net.DialTimeout("tcp", c.address, c.timeout)
+		if err != nil {
+			fmt.Println("Ошибка подключения:", err)
+			return err
+		}
+		c.conn = conn
+	} else {
+		fmt.Println("Соединение уже установлено")
+	}
+	return nil
+}
+
+func (c *TCPClient) Send() error {
+	if c.conn == nil {
+		return fmt.Errorf("не было произведено подключение к серверу. Используйте функцию Connect()")
+	}
+	_, err := io.Copy(c.conn, c.in)
+	return err
+}
+
+func (c *TCPClient) Receive() error {
+	if c.conn == nil {
+		return fmt.Errorf("не было произведено подключение к серверу. Используйте функцию Connect()")
+	}
+	_, err := io.Copy(c.out, c.conn)
+	return err
+}
+
+func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
+	client := &TCPClient{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+	return client
 }
 
 // Place your code here.
