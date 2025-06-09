3708fb284aa561b41ed7c3760a94e79ef1fefbbe
Yulia Prusova <prusova108@gmail.com>
2025-06-09 22:55:52 +0300

    test

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -9,62 +9,6 @@ import (
 	"time"
 )
 
-func close_telnet_client(client TelnetClient, msg string) {
-	fmt.Println(msg)
-	client.Close()
-}
-
-func send_receive(telnet_client TelnetClient) context.Context {
-	ctx, cancel := context.WithCancel(context.Background())
-	go func() {
-		fmt.Println("Старт работы клиента")
-		for {
-			err := telnet_client.Send()
-			if err != nil {
-				close_telnet_client(telnet_client, err.Error())
-				cancel()
-				return
-			}
-			err = telnet_client.Receive()
-			if err != nil {
-				close_telnet_client(telnet_client, err.Error())
-				cancel()
-				return
-			}
-			time.Sleep(1 * time.Second)
-		}
-	}()
-	return ctx
-}
-
-// func main() {
-// 	c := make(chan os.Signal, 1)
-// 	signal.Notify(c, syscall.SIGINT)
-
-// 	ctx, cancel := context.WithCancel(context.Background())
-
-// 	go func() {
-// 		<-c
-// 		fmt.Println("Получен сигнал SIGINT")
-// 		cancel()
-// 	}()
-
-// 	telnet_client := NewTelnetClient("localhost:4242", 10*time.Second, os.Stdin, os.Stdout)
-// 	if err := telnet_client.Connect(); err != nil {
-// 		fmt.Println(err)
-// 		os.Exit(1)
-// 	}
-
-// 	err_ctx := send_receive(telnet_client)
-
-// 	select {
-// 	case <-err_ctx.Done():
-// 		return
-// 	case <-ctx.Done():
-// 		close_telnet_client(telnet_client, "Выполнение программы завершено")
-// 	}
-// }
-
 func main() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGINT)
@@ -77,18 +21,21 @@ func main() {
 		cancel()
 	}()
 
-	telnet_client := NewTelnetClient("localhost:4242", 10*time.Second, os.Stdin, os.Stdout)
-	if err := telnet_client.Connect(); err != nil {
+	telnetClient := NewTelnetClient("localhost:4242", 10*time.Second, os.Stdin, os.Stdout)
+	if err := telnetClient.Connect(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	err_ctx := run_client(telnet_client)
+	sendCtx, receiveCtx := run_client(telnetClient)
 
 	select {
-	case <-err_ctx.Done():
+	case <-sendCtx.Done():
+		return
+	case <-receiveCtx.Done():
 		return
 	case <-ctx.Done():
-		close_telnet_client(telnet_client, "Выполнение программы завершено")
+		fmt.Println("Выполнение программы завершено")
 	}
+	telnetClient.Close()
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -8,6 +8,14 @@ import (
 	"time"
 )
 
+type ClientClosedError struct {
+	Message string
+}
+
+func (e *ClientClosedError) Error() string {
+	return e.Message
+}
+
 type TelnetClient interface {
 	Connect() error
 	io.Closer
@@ -16,12 +24,12 @@ type TelnetClient interface {
 }
 
 type TCPClient struct {
-	conn         net.Conn
-	conn_scanner bufio.Scanner
-	in           bufio.Scanner
-	out          bufio.Writer
-	address      string
-	timeout      time.Duration
+	conn       net.Conn
+	connReader bufio.Reader
+	in         bufio.Reader
+	out        bufio.Writer
+	address    string
+	timeout    time.Duration
 }
 
 func (c *TCPClient) Close() error {
@@ -40,7 +48,7 @@ func (c *TCPClient) Connect() error {
 		}
 		fmt.Println("Соединение установлено")
 		c.conn = conn
-		c.conn_scanner = *bufio.NewScanner(conn)
+		c.connReader = *bufio.NewReader(conn)
 	} else {
 		fmt.Println("Соединение уже установлено")
 	}
@@ -49,7 +57,7 @@ func (c *TCPClient) Connect() error {
 
 func (c *TCPClient) write(message []byte, to io.Writer) error {
 	sent_bytes := 0
-	message = append(message, []byte("\n")...)
+	// message = append(message, []byte("\n")...)
 	for sent_bytes < len(message) {
 		n, err := to.Write(message[sent_bytes:])
 		if err != nil {
@@ -64,12 +72,20 @@ func (c *TCPClient) Send() error {
 	if c.conn == nil {
 		return fmt.Errorf("не было произведено подключение к серверу. Используйте функцию Connect()")
 	}
-	fmt.Println("Введите сообщение:")
-	if c.in.Scan() {
-		message := c.in.Bytes()
-		return c.write(message, c.conn)
+	// fmt.Println("Введите сообщение:")
+	// if c.in.Scan() {
+	// 	message := c.in.Bytes()
+	// 	return c.write(message, c.conn)
+	// }
+	// err := c.in.Err()
+	text, err := c.in.ReadString('\n')
+	fmt.Printf("Send err: %v\n", err)
+	if err == io.EOF || err == io.ErrUnexpectedEOF {
+		return &ClientClosedError{Message: "Соединение закрыто клиентом"}
+	} else if err != nil {
+		return err
 	}
-	err := c.in.Err()
+	err = c.write([]byte(text), c.conn)
 	return err
 }
 
@@ -77,22 +93,33 @@ func (c *TCPClient) Receive() error {
 	if c.conn == nil {
 		return fmt.Errorf("не было произведено подключение к серверу. Используйте функцию Connect()")
 	}
-	if c.conn_scanner.Scan() {
-		message := c.conn_scanner.Bytes()
-		err := c.write(message, &c.out)
-		if err != nil {
-			return err
-		}
-		return c.out.Flush()
+	// if c.connScanner.Scan() {
+	// 	message := c.connScanner.Bytes()
+	// 	err := c.write(message, &c.out)
+	// 	if err != nil {
+	// 		return err
+	// 	}
+	// 	return c.out.Flush()
+	// }
+	// return c.connScanner.Err()
+	text, err := c.connReader.ReadString('\n')
+	fmt.Printf("Recieve err: %v\n", err)
+	if err != nil {
+		return err
 	}
-	return c.conn_scanner.Err()
+	err = c.write([]byte(text), &c.out)
+	if err != nil {
+		return err
+	}
+	err = c.out.Flush()
+	return err
 }
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
 	client := &TCPClient{
 		address: address,
 		timeout: timeout,
-		in:      *bufio.NewScanner(in),
+		in:      *bufio.NewReader(in),
 		out:     *bufio.NewWriter(out),
 	}
 	return client
